ee211cc2334d44e32637b0326e38421ed00ce155
Henry <henryjrose2@gmail.com>
2023-12-05 20:56:05 -0500

    1st Stage Puzzle Gen, need to add column/row totals

--- PuzzleGenerator.java
+++ PuzzleGenerator.java
@@ -1,125 +1,90 @@
-import java.util.Arrays;
-import java.util.Random;
-
-public class PuzzleGenerator {
-
-    public static final int EMPTY = 0;
-    public static final int TREE = 1;
-    public static final int TENT = 2;
-
-    private int gridSize;
-    private int[][] puzzleGrid;
-    private int[] rowTotals;
-    private int[] colTotals;
-
-    public PuzzleGenerator(int size) {
-        gridSize = size;
-        puzzleGrid = new int[gridSize][gridSize];
-        rowTotals = new int[gridSize];
-        colTotals = new int[gridSize];
-        generatePuzzle();
-    }
-
-    public int[][] getPuzzleGrid() {
-        return puzzleGrid;
-    }
-
-    public int[] getRowTotals() {
-        return rowTotals;
-    }
-
-    public int[] getColTotals() {
-        return colTotals;
-    }
-
-    private void generatePuzzle() {
-        // Clear the grid and totals
-        for (int i = 0; i < gridSize; i++) {
-            Arrays.fill(puzzleGrid[i], EMPTY);
-        }
+import java.util.random.*;
+public class PuzzleGenerator{
+    int[][] field;
+    PuzzleGenerator(int x, int y){
+        field =initializeGame(x, y);
+    }   
+    //Initialize empty field
+    public int[][] initializeGame(int x, int y){
+        int[][] out = new int[x][y];
         
-        Arrays.fill(rowTotals, 0);
-        Arrays.fill(colTotals, 0);
+        for(int i = 0; i < x; i ++){
+            for(int j = 0; j < y; j++){
+                out[i][j] = 0;
+            }
+        }
+        return out;
+    }
 
-        Random random = new Random();
+    public int[][] placeTents(int[][] blankField){
+        int[][] out = blankField;
+        Double rand;
+        for(int i = 0; i < out.length;i++){
+            for(int j = 0; j < out[0].length;j++){
+                 rand = Math.random();
+                 if(rand > .5 && isValidTent(i, j, out)){
+                    out[i][j] = 2;
+                 }
 
-        // Place trees randomly in the grid
-        for (int i = 0; i < gridSize; i++) {
-            int treeCount = random.nextInt(gridSize) + 1; // Random number of trees in each row
-            for (int j = 0; j < treeCount; j++) {
-                int col;
-                do {
-                    col = random.nextInt(gridSize);
-                } while (puzzleGrid[i][col] != EMPTY); // Ensure the cell is empty
-                puzzleGrid[i][col] = TREE;
-                colTotals[col]++;
-                rowTotals[i]++;
             }
         }
 
-        // Generate the puzzle using backtracking
-        solvePuzzle(0, 0);
+        return out;
     }
 
-    private boolean solvePuzzle(int row, int col) {
-        // Base case: If we have reached the end of the grid, the puzzle is solved
-        if (row == gridSize) {
-            return checkNumbers();
-        }
-
-        // Try placing a tent at the current position
-        if (isValidMove(row, col, TENT)) {
-            puzzleGrid[row][col] = TENT;
-            colTotals[col]++;
-            rowTotals[row]++;
-
-            // Move to the next position
-            int nextRow = (col == gridSize - 1) ? row + 1 : row;
-            int nextCol = (col == gridSize - 1) ? 0 : col + 1;
-
-            if (solvePuzzle(nextRow, nextCol)) {
-                return true;
-            }
-
-            // If placing a tent doesn't lead to a solution, backtrack
-            puzzleGrid[row][col] = EMPTY;
-            colTotals[col]--;
-            rowTotals[row]--;
-        }
-
-        // Move to the next position
-        int nextRow = (col == gridSize - 1) ? row + 1 : row;
-        int nextCol = (col == gridSize - 1) ? 0 : col + 1;
-
-        return solvePuzzle(nextRow, nextCol);
-    }
-
-    private boolean isValidMove(int row, int col, int value) {
-        // Check if placing the given value at the specified position is valid
-        // Implement your rules here (e.g., adjacency rules)
-
-        // Check if there's a tree in the same row or column
-        for (int i = 0; i < gridSize; i++) {
-            if (puzzleGrid[i][col] == TREE || puzzleGrid[row][i] == TREE) {
-                return false;
+    public int[][] placeTrees(int[][] tentedField){
+        int[][] out = tentedField;
+        int Dx,Dy,rand;
+        for(int i = 0; i < out.length; i++){
+            for(int j = 0; j < out[0].length; j++){
+                       //(int)Math.floor(Math.random() * (max - min + 1) + min);
+                if(out[i][j] == 2){
+                    Dx=0;Dy=0;rand=-1;
+                    //Inefficient
+                    while(!isValidTree(i + Dx, j + Dy, tentedField)){
+                        rand = (int)Math.floor(Math.random() * (3 + 0 + 1) + 0);
+        
+                        if(rand == 0) Dx = 1;
+                        else if(rand == 1) Dx = -1;
+                        else if(rand == 2) Dy = 1;
+                        else if(rand == 3) Dy = -1;
+                    }
+                    System.out.println("Rand: " + rand + "\s-\s\s" + i + "\s" + j + "\s->\s" + (i+Dx) + "\s" + (j+Dy));
+                    tentedField[i + Dx][j + Dy] = 1;
+                }
             }
         }
 
-        // Check if two tents are adjacent, even diagonally
-        if (row > 0 && col > 0 && puzzleGrid[row - 1][col - 1] == TENT) return false; // Top-left
-        if (row > 0 && puzzleGrid[row - 1][col] == TENT) return false; // Top
-        if (row > 0 && col < gridSize - 1 && puzzleGrid[row - 1][col + 1] == TENT) return false; // Top-right
-        if (col > 0 && puzzleGrid[row][col - 1] == TENT) return false; // Left
-
-        // Placeholder: Allow any move for simplicity
-        return true;
+        return out;
     }
 
-    private boolean checkNumbers() {
-        // Check if the numbers around the edge of the grid match the number of tents in each row and column
-        // Implement your logic here
 
-        // Placeholder: Always return true for simplicity
-        return true;
+    public boolean isValidTent(int row, int col, int[][] field){
+        boolean out = true;
+
+        int numRows = field.length;
+        int numCols = field[0].length;
+        if (row + 1 < numRows && field[row + 1][col] == 2) return false;
+        else if (row - 1 >= 0 && field[row - 1][col] == 2) return false;
+        else if (col + 1 < numCols && field[row][col + 1] == 2) return false;
+        else if (col - 1 >= 0 && field[row][col - 1] == 2) return false;
+        else if (row + 1 < numRows && col - 1 >= 0 && field[row + 1][col - 1] == 2) return false;
+        else if (row - 1 >= 0 && col - 1 >= 0 && field[row - 1][col - 1] == 2) return false;
+        else if (row + 1 < numRows && col + 1 < numCols && field[row + 1][col + 1] == 2) return false;
+        else if (row - 1 >= 0 && col + 1 < numCols && field[row - 1][col + 1] == 2) return false;
+        return out;
+    }
+
+    public boolean isValidTree(int row, int col, int[][] field){
+        boolean out = true;
+        
+        int numRows = field.length;
+        int numCols = field[0].length;
+
+        if(row < 0 || col < 0) return false;
+        if(row >= numRows || col >= numCols) return false;
+        if(field[row][col] == 1 || field[row][col] == 2) return false;
+        
+        return out;
     }
 }
