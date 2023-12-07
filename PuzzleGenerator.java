b93846688bbaa878285dfff5326f6d7b80511772
Henry <henryjrose2@gmail.com>
2023-12-07 17:11:54 -0500

    Working game

--- PuzzleGenerator.java
+++ PuzzleGenerator.java
@@ -2,7 +2,7 @@ import java.util.random.*;
 public class PuzzleGenerator{
     int[][] field;
     PuzzleGenerator(int x, int y){
-        field =initializeGame(x, y);
+        field = placeTrees(placeTents(initializeGame(x, y)));
     }   
     //Initialize empty field
     public int[][] initializeGame(int x, int y){
@@ -39,17 +39,17 @@ public class PuzzleGenerator{
             for(int j = 0; j < out[0].length; j++){
                        //(int)Math.floor(Math.random() * (max - min + 1) + min);
                 if(out[i][j] == 2){
-                    Dx=0;Dy=0;rand=-1;
+                    Dx=0;Dy=0;
                     //Inefficient
                     while(!isValidTree(i + Dx, j + Dy, tentedField)){
+                        Dx=0;Dy=0;rand=-1;
                         rand = (int)Math.floor(Math.random() * (3 + 0 + 1) + 0);
-        
                         if(rand == 0) Dx = 1;
                         else if(rand == 1) Dx = -1;
                         else if(rand == 2) Dy = 1;
                         else if(rand == 3) Dy = -1;
                     }
-                    System.out.println("Rand: " + rand + "\s-\s\s" + i + "\s" + j + "\s->\s" + (i+Dx) + "\s" + (j+Dy));
+                    //System.out.println("Rand: " + rand + "\s-\s\s" + i + "\s" + j + "\s->\s" + (i+Dx) + "\s" + (j+Dy));
                     tentedField[i + Dx][j + Dy] = 1;
                 }
             }
@@ -58,7 +58,21 @@ public class PuzzleGenerator{
         return out;
     }
 
+    public int[][] getInitialGameState(int[][] field){
+        int[][] out = new int[field.length][field[0].length];
+        for(int i = 0; i < out.length; i++){
+            for(int j = 0; j < out[0].length; j++){
+                out[i][j] = field[i][j];
+            }
+        }
 
+        for(int i = 0; i < out.length; i++){
+            for(int j = 0; j < out.length; j++){
+                if(out[i][j] == 2) out[i][j] = 0;
+            }
+        }
+        return out;
+    }
     public boolean isValidTent(int row, int col, int[][] field){
         boolean out = true;
 
@@ -76,6 +90,7 @@ public class PuzzleGenerator{
     }
 
     public boolean isValidTree(int row, int col, int[][] field){
+        System.out.println("Invalid");
         boolean out = true;
         
         int numRows = field.length;
@@ -85,6 +100,27 @@ public class PuzzleGenerator{
         if(row >= numRows || col >= numCols) return false;
         if(field[row][col] == 1 || field[row][col] == 2) return false;
         
+        return out;
+    }
+
+    public int[][] getRowTotals(int x, int y, int[][] field){
+
+        int[][] out = new int[2][x];
+        for(int l = 0; l < 2; l++){
+            for(int k = 0; k < x; k++){
+                out[l][k] = 0;
+            }
+        }
+
+        for(int i = 0; i < x; i++){
+            for(int j = 0; j < y; j++){
+                if(field[i][j] == 2){
+                    out[0][j]++;
+                    out[1][i]++;
+                }
+            }
+        }
+
         return out;
     }
 }
--- TentsAndTreesPuzzle.java
+++ TentsAndTreesPuzzle.java
@@ -4,13 +4,16 @@ import javax.xml.xpath.XPathExpressionException;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.text.Format.Field;
 
 public class TentsAndTreesPuzzle extends JFrame{
 
     private JButton[][] gridButtons;
     private JLabel[][] totals;  
-    private int gridSize = 5; // Change the grid size as needed
+    private static int gridSize = 3; // Change the grid size as needed
     private int[][] solution;
+    private int[][] gameState;
 
     private static final String TREE_IMAGE_PATH = "./assets/tree-icon.png";
     private static final String TENT_IMAGE_PATH = "./assets/tent-icon.png";
@@ -21,27 +24,63 @@ public class TentsAndTreesPuzzle extends JFrame{
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         PuzzleGenerator pg = new PuzzleGenerator(gridSize, gridSize);
         solution = pg.field; // 0: Empty, 1: Tree, 2: Tent
-
-    }
-    public static void main(String[] args) {
+        gameState = pg.getInitialGameState(solution);
+        totals = createTotals(pg.getRowTotals(gridSize, gridSize, solution));
+        for(int i = 0; i < gridSize; i++){
+            for(int j = 0; j < gridSize; j++){
+                if(gameState[i][j] == 2) gameState[i][j] = 0;
+            }
+        }
+        createGridButtons(pg, gameState);
         
-        PuzzleGenerator pg = new PuzzleGenerator(5, 5);
-        int[][] field = pg.field;
-        printField(field);
+                // Set up the layout
+         JPanel gridPanel = new JPanel(new GridLayout(gridSize, gridSize));
+         for (int i = 0; i < gridSize; i++) {
+             for (int j = 0; j < gridSize; j++) {
+                 gridPanel.add(gridButtons[i][j]);
+             }
+         }
 
-        System.out.println();
+         JPanel totalsPanel = new JPanel(new GridLayout(gridSize + 1, gridSize + 1)); // +1 for row and column totals
 
-        field = pg.placeTents(field);
-        printField(field);
+         // Add column totals
+         totalsPanel.add(new JLabel());  // Empty corner cell
+         for (int i = 0; i < gridSize; i++) {
+             System.out.println(totals[0][i]);
+             totalsPanel.add(totals[0][i]);
+         }
 
-        System.out.println();
+         // Add row and grid cells
+         for (int i = 0; i < gridSize; i++) {
+             totalsPanel.add(totals[1][i]);  // Row total
+             for (int j = 0; j < gridSize; j++) {
+                 totalsPanel.add(gridButtons[i][j]);
+             }
+         }
 
-        field = pg.placeTrees(field);
-        printField(field);
-        System.out.println("\n\n\n\n\n\n\n\n\n");
-        printSolution(field);
+         setLayout(new BorderLayout());
+         add(gridPanel, BorderLayout.CENTER);
+         add(totalsPanel, BorderLayout.SOUTH);
 
+         pack();
+         setLocationRelativeTo(null); // Center the frame
+         setVisible(true);
     }
+    private JLabel[][] createTotals(int[][] pgTotals) {
+        printSolution(pgTotals);
+        // Create a 2D array for both row and column totals
+        totals = new JLabel[2][gridSize];
+        for(int i = 0; i < 2;i++){
+            for(int j = 0; j < gridSize; j++){
+                totals[i][j] = new JLabel(String.valueOf(pgTotals[i][j]));
+                System.out.println(pgTotals[i][j]);
+            }
+        }
+        return totals;
+    }
+
+
+    
 
     public static void printSolution(int[][] field){
         for(int i = 0; i < field.length; i++){
@@ -61,6 +100,116 @@ public class TentsAndTreesPuzzle extends JFrame{
         }
     }
 
+    private void createGridButtons(PuzzleGenerator pg, int[][] field) {
+        gridButtons = new JButton[gridSize][gridSize];
+
+        for (int i = 0; i < gridSize; i++) {
+            for (int j = 0; j < gridSize; j++) {
+                JButton button = new JButton();
+
+                // Load and resize the tree image for cells with a tree
+                if (field[i][j] == 1) {
+                    ImageIcon treeIcon = resizeImageIcon(TREE_IMAGE_PATH, 50, 50);
+                    button.setIcon(treeIcon);
+                }
+                else if(field[i][j] == 2){
+                    ImageIcon tentIcon = resizeImageIcon(TENT_IMAGE_PATH, 30, 30);
+                    button.setIcon(tentIcon);
+                }
+
+                Color brownColor = new Color(139, 69, 19);
+                button.setBackground(brownColor);
+                button.setContentAreaFilled(false); // Make the content area transparent
+                button.setOpaque(true); // Make sure the button is opaque
+                button.setPreferredSize(new Dimension(50, 50));
+
+                final int row = i;
+                final int col = j;
+
+                
+                button.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        // Add handleButtonClick
+      
+                        handleButtonClick(pg, row, col, field);
+                    }
+                });
+                
+
+                gridButtons[i][j] = button;
+            }
+        }
+    }
+    private void handleButtonClick(PuzzleGenerator pg, int row, int col, int[][] field) {
+        if (field[row][col] == 2) {
+
+            // Tent already placed, remove it
+            field[row][col] = 0;
+            gridButtons[row][col].setIcon(null);
+            solution[row][col] = 0;
+        } else if (pg.isValidTent(row, col, field) && isNextToTree(row, col, solution)) {
+            // Place a tent
+            ImageIcon tentIcon = resizeImageIcon(TENT_IMAGE_PATH, 30, 30);
+            gridButtons[row][col].setIcon(tentIcon);
+            field[row][col] = 2;
+            
+           
+        }
+
+        if(checkForWin(solution, field)) {System.out.println("WIN"); System.exit(0);};
+        
+        //check for win
+    }
+    private boolean isNextToTree(int row, int col, int[][] field){
+
+        if(row + 1 < gridSize && field[row + 1][col] == 1) return true;
+        else if(col -1  >= 0 && field[row][col - 1] == 1) return true;
+        else if(col + 1 < gridSize && field[row][col + 1] == 1) return true;
+        else if(row - 1 >= 0 && field[row - 1][col] == 1) return true;
+        return false;
+    }
+
+    private boolean checkForWin(int[][] solution, int[][] field){
+        for(int i = 0; i < gridSize; i++){
+            for(int j = 0; j < gridSize; j++){
+                if(solution[i][j] != field[i][j]){System.out.println("Solution " + solution[i][j] + "Index "
+                 + i + j + "Field " + field[i][j] + "Index " + i + j); return false;
+            }}
+        }
+        System.out.println("Win");
+        return true;
+        
+
+    }
+
+
+    private ImageIcon resizeImageIcon(String imagePath, int width, int height) {
+        ImageIcon icon = new ImageIcon(imagePath);
+        Image image = icon.getImage();
+
+        // Calculate the aspect ratio
+        double aspectRatio = (double) image.getWidth(null) / image.getHeight(null);
+
+        // Calculate the new width based on the aspect ratio
+        int newWidth = (int) (height * aspectRatio);
+
+        // Resize the image
+        Image scaledImage = image.getScaledInstance(newWidth, height, Image.SCALE_SMOOTH);
+        return new ImageIcon(scaledImage);
+    }
+
+
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new TentsAndTreesPuzzle();
+            }
+        });
+
+    }
 
 
     
